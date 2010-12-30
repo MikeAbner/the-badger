0d7977e982f952faf4cccb70b5c6a2de3462e884
Mike Abner <mike.abner@gmail.com>
2010-12-29 17:14:32 -0800

    a new clean rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BadgerBadger::Application.config.secret_token = '8923ce77b1e579f15ab9e233cd9f84c264425978aa1774369e0806c6dafda74f962c51ebe607c50d9fe0deddc0e93a4ed04bab6f6a193d540e8c934d2967018c'
